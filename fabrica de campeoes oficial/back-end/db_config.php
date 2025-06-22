cf4dafbe69e4e2e2d6d24ade97ff95b079407fb4
Builtter <dego.dvl@gmail.com>
2025-06-22 20:40:11 -0300

    Troca nome DB

--- fabrica de campeoes oficial/back-end/db_config.php
+++ fabrica de campeoes oficial/back-end/db_config.php
@@ -2,6 +2,6 @@
 define('DB_SERVERNAME', 'localhost');
 define('DB_USERNAME', 'root');
 define('DB_PASSWORD', '');
-define('DB_NAME', 'fabricadecampeoes');
+define('DB_NAME', 'fabrica_db');
 define('DB_PORT', '3306');
 ?>
